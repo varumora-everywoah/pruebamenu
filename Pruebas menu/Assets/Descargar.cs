99e56fbfba561a5202471b0a1e053f770b27474a
varumora <varu.mora@gmail.com>
2016-12-20 10:07:08 +0100

    Primer commit
    Pos eso

---
+++ Pruebas menu/Assets/Descargar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class Descargar : MonoBehaviour {
+	WWW www;
+
+	public Text deb;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("descarga");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (""+www.bytesDownloaded);
+		
+	}
+
+	IEnumerator descarga(){
+		www= new WWW ("https://storage.googleapis.com/vr-city/cloud/ios/vrcityshowreel_ios_2096_42_2.mp4");
+		while (!www.isDone) {
+			deb.text= ("downloaded " + (www.progress * 100).ToString () + "%...");
+			yield return null;
+		}
+
+		string fullPath = Application.persistentDataPath + "/vid.mp4";
+		File.WriteAllBytes (fullPath, www.bytes);
+	}
+}
